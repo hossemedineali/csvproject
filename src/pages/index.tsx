82f24318ab31037e4f94ee563276a091c9cde157
hossemedineali <hossemedine@outlook.com>
2023-03-09 02:44:49 +0100

    5

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,8 +3,11 @@ import { type NextPage } from "next";
 import Head from "next/head";
 import Link from "next/link";
 
+import { trpc } from "@/utils/trpc";
 
 const Home: NextPage = () => {
+  const x=trpc.hello.useQuery({text:'hosse!'})
+  console.log(x.data)
 
   return (
     <>
